035ca9f65b4230656b0c992184555b08cff11a05
Manoa Rasolomalala <manoatony@gmail.com>
2022-05-20 16:10:20 +0200

    asset and js w/ view _modal

--- app/javascript/controllers/tom_select_controller.js
+++ app/javascript/controllers/tom_select_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
-import TomSelect from "tom-select"
+import TomSelect from "select"
 
-// Connects to data-controller="tom-select"
+// Connects to data-controller="select"
 export default class extends Controller {
   connect() {
     new TomSelect(this.element, {})
